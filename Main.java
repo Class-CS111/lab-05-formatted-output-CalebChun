091db99853f238541a80e286974f59d7dd79ef64
CalebChun <1024656@oside.us>
2025-04-04 11:27:10 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,11 +7,18 @@ public class Main
 	public static void main(String[] args)
 	{
     //DECLARATION + INITIALIZATION SECTION
-
+	int initialCents = 141; 
+        int remainingCents = initialCents;
     //CALCULATION SECTION
-
+	int quarters = remainingCents / 25;
+        remainingCents %= 25;
+        int dimes = remainingCents / 10;
+        remainingCents %= 10;
+        int nickels = remainingCents / 5;
+        remainingCents %= 5;
+        int pennies = remainingCents;
     //OUTPUT SECTION
-
+	System.out.println("You have "+quarters+" quarters, "+dimes+" dimes, "+nickels+" nickels, and "+pennies+" pennies!");
   }
 
 }
